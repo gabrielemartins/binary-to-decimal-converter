f02ec3f46194acd1426c2056223bfe8fa1d98a32
Gabriele <gabriieele6@gmail.com>
2022-05-18 10:44:05 -0300

    feat: check if input value has correct pattern

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const binaryInput = document.querySelector("input");
+
 window.onload = function () {
     binaryInput.value = "";
 };
+
+function checkPattern(regex, value) {
+    const pattern = regex;
+    return pattern.test(value);
+}
