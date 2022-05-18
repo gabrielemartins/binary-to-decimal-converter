7f700219bc6a7bfbaf1e7ab19b59e90e65e3233c
Gabriele <gabriieele6@gmail.com>
2022-05-18 10:47:18 -0300

    feat: convert binary number to decimal

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const binaryInput = document.querySelector("input");
+const result = document.querySelector(".result-text");
+const error = document.querySelector(".error");
 
 window.onload = function () {
     binaryInput.value = "";
@@ -7,4 +9,22 @@ window.onload = function () {
 function checkPattern(regex, value) {
     const pattern = regex;
     return pattern.test(value);
+}
+
+function binaryToDecimal() {
+    let binaryNumber = binaryInput.value;
+    const checkedPattern = checkPattern(/^[0-1]+$/, binaryNumber);
+
+    if (checkedPattern || binaryNumber === "") {
+        let binaryNumberSize = binaryInput.value.length - 1;
+        let power = 0;
+        let total = 0;
+        for (let index = binaryNumberSize; index >= 0; index--) {
+            let binaryNumberPart = binaryNumber.charAt(index);
+            total += Math.pow(2, power) * binaryNumberPart;
+            power++;
+        }
+        result.innerHTML = "result: " + total;
+        error.innerHTML = "";
+    }
 }
